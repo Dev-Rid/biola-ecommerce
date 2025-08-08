067632cac59d9407073c33229d8c2baf5e259ccd
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-08-08 20:49:26 +0100

    feat: UI folder created with loading and button files plus App.jsx modified...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './contexts/AuthContext'
+import { CartProvider } from './contexts/CartContext'
+import Layout from './components/Layout/Layout'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+import './index.css'
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:id" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </Layout>
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: '#363636',
+                color: '#fff',
+              },
+              success: {
+                duration: 3000,
+                theme: {
+                  primary: '#4aed88',
+                },
+              },
+            }}
+          />
+        </Router>
+      </CartProvider>
+    </AuthProvider>
   )
 }
 
