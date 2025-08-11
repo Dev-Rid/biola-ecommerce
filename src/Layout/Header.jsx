ea3a297ab6b4017bfc6525b19d8b9aff1890971c
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-08-11 23:58:33 +0100

    name changed to biolastore

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -25,9 +25,9 @@ const Header = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
             <div className="w-8 h-8 bg-primary-600 rounded-lg flex items-center justify-center">
-              <span className="text-white font-bold text-lg">B</span>
+              <span className="text-white font-bold text-lg">Biola</span>
             </div>
-            <span className="text-xl font-bold text-red-500">EStore</span>
+            <span className="text-xl font-bold text-red-500">Store</span>
           </Link>
 
           {/* Desktop Navigation */}
